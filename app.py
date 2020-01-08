b1b57904643496f15398fd129f236519e8cabd19
DK Lee <mickeyfromsd@gmail.com>
2020-01-08 00:55:04 -0800

    fix indentation

--- app.py
+++ app.py
@@ -16,15 +16,11 @@ def verify():
         if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
             return "Verification token mismatch", 403
         return request.args["hub.challenge"], 200
-
     return "Hello world", 200
-
 
 @app.route('/', methods=['POST'])
 def webhook():
-
     # endpoint for processing incoming messaging events
-
     data = request.get_json()
     log(data)  # you may not want to log every incoming message in production, but it's good for testing
 
@@ -40,41 +36,28 @@ def webhook():
                     message_text = messaging_event["message"]["text"]  # the message's text
                     # Test for Fiance search result
 					#https://finance.yahoo.com/quote/GLD?p=GLD&.tsrc=fin-srch-v1
-					#
-					#
-					
-                    yahoofinance = "https://finance.yahoo.com/quote/"
-					inter3="?p="
-					inter4="&.tsrc=fin-srch-v1"
-					
+                    yahoofinance="https://finance.yahoo.com/quote/"
+                    inter3="?p="
+                    inter4="&.tsrc=fin-srch-v1"
                     consult="https://www.stockconsultant.com/consultnow/basicplus.cgi?symbol="
                     seeking_alpha="https://seekingalpha.com/symbol/"
                     inter2="?s="
                     space="\n\n"
-                    
                     quote_consult=consult+message_text                    
                     quote_sa=seeking_alpha+message_text+inter2+message_text
-                    quote_yahoo=yahoofinance+message_text+inter3+message+inter4
+                    quote_yahoo=yahoofinance+message_text+inter3+message_text+inter4
                     quote=quote_consult+space+quote_sa+space+quote_yahoo
-                    
                     send_message(sender_id,quote)
-
                 if messaging_event.get("delivery"):  # delivery confirmation
                     pass
-
                 if messaging_event.get("optin"):  # optin confirmation
                     pass
-
                 if messaging_event.get("postback"):  # user clicked/tapped "postback" button in earlier message
                     pass
-
     return "ok", 200
 
-
 def send_message(recipient_id, message_text):
-
     log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
-
     params = {
         "access_token": os.environ["PAGE_ACCESS_TOKEN"]
     }
