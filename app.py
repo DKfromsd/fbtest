d449d28618b7413cd855f3ebca48094067261bd0
DoKyu Lee <dokyulee@DK-MacBook-Air.local>
2024-11-17 00:14:38 -0800

    back

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 import sys
 import json
 
-import credentials, requests
+import requests
 from flask import Flask, request
 
 app = Flask(__name__)
@@ -15,7 +15,6 @@ def verify():
     if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
         if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
             # os.environ.get("VERIFY_TOKEN", "default_verify_token")
-
             return "Verification token mismatch", 403
         return request.args["hub.challenge"], 200
     return "Hello world", 200
